0fd7188a237ca6bd04c6c1af7ca66a3f753ec9b5
John1001Song <jinyuesong88@gmail.com>
2019-06-10 00:19:02 -0700

    simplify tree build algorithm

--- analysis_tool_python/forked_chain/tree_builder.py
+++ analysis_tool_python/forked_chain/tree_builder.py
@@ -2,21 +2,20 @@
 # Object Blocks loads all blocks and these blocks don't have relationship yet.
 # First, build relationship.
 #   (always current block's height - 1 = current block parent height)
-#   global root locates at height 6355788, where we start to download
+#   global root locates at height 6355788, where we start to download; its parent block is None
 #   while current has child level:
-#       one iteration keeps blocks from three continue heights
-#
-#       blocks have three status: white, grey and black
-#       when a block is first touched, which is defined as a child of previous height block, it is white status
-#       when a block is iterated as current block, it is grey status
-#       when a block is defined as the next block's parent, it is black
-#
-#       iterate every three heights, increasing by 1
-#       update current height block's parent, children, peers
-#       black is updated as grey parent,
-#       white is update as grey children
-#       change status white -> grey -> black
-#       read blocks from next height and define as white status
+#       one iteration keeps blocks from two continue heights
+##
+#       iterate every two heights, increasing by 1
+#       current_blocks at current height
+#       child_blocks at next height
+#       update vertical relationship:
+#           update current_blocks as child_blocks' parent
+#           update child_blocks as current_blocks' children
+#       update horizontal relationship:
+#           update current blocks at the same height as peers
+#       current_blocks = children blocks
+#       read next height as children blocks
 #
 #       repeat iterations until last height, which has no children and child = None
 
