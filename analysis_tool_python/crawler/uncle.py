109596f055ddfbf9e974a9f783393ca3f3c661e2
CharlesGe129 <char1209ge@gmail.com>
2019-07-04 21:03:40 +0800

    Add ugly try-catch

--- analysis_tool_python/crawler/uncle.py
+++ analysis_tool_python/crawler/uncle.py
@@ -6,6 +6,26 @@ from datetime import datetime
 bs = BeautifulSoup
 
 
+def extract_detail_page(table):
+    info = {}
+    divs = table.find_all('div', {'class': 'col-md-9'})
+    info['uncleHeight'] = divs[0].find('strong').string
+    info['unclePosition'] = divs[1].string.strip('\n')
+    info['blockHeight'] = divs[2].find('a').string
+    info['hash'] = divs[3].string.strip('\n')
+    info['parentHash'] = divs[4].string.strip('\n')
+    info['sha3Uncles'] = divs[5].string.strip('\n')
+    info['minerHash'] = divs[6].find('a').string.split(' ')[0]
+    info['miner'] = divs[6].find('a').string.split(' ')[1] if len(divs[6].find('a').string.split(' ')) > 1 else ''
+    info['mineTime'] = divs[6].text.split('in ')[1].split(' secs')[0]
+    info['difficulty'] = divs[7].string.strip('\n')
+    info['gasLimit'] = divs[8].string.strip('\n')
+    info['gasUsed'] = divs[9].string.strip('\n')
+    info['timeStampUnformated'] = str(divs[10]).split('\n')[-2].split('(')[1].strip(')')
+    info['uncleReward'] = str(divs[11]).split('\n')[-2].replace('<b>', '').replace('</b>', '')
+    return info
+
+
 class UncleCrawler:
     def __init__(self):
         self.etherscan_url = 'https://etherscan.io'
@@ -17,54 +37,44 @@ class UncleCrawler:
         counter = 1
         i = 1
         while i <= 8963:
-        # for i in range(1, 8963):
-            print(f'loading page {i}')
             url = f"{self.list_url_prefix}{i}"
-            print(f"loading {url}")
+            print(f'loading page {i}: {url}')
             detail_page_urls = self.load_list_page(url)
             for detail_url in detail_page_urls:
                 url = self.etherscan_url + detail_url
                 info = self.load_detail_page(url)
                 self.save(info)
-                print(f"Average time for {counter} blocks: {((datetime.now()-started_at).seconds) / counter} seconds")
+                print(f"Average time for {counter} blocks: {((datetime.now() - started_at).seconds) / counter} seconds")
                 counter += 1
             i += 1
 
     @staticmethod
     def load_list_page(url):
-        content = requests.get(url).content
-        soup = BeautifulSoup(content, 'html.parser')
-        table = soup.find('table', {'class': 'table table-hover'})
-        rows = table.find('tbody').find_all('tr')
-        # 返回的lamdba表达式的详细内容
-        # for row in rows:
-        #     uncle_td = row.find_all('td')[1]
-        #     href = uncle_td.find('a')['href']
-        return [row.find_all('td')[1].find('a')['href'] for row in rows]
+        while True:
+            try:
+                content = requests.get(url).content
+                soup = BeautifulSoup(content, 'html.parser')
+                table = soup.find('table', {'class': 'table table-hover'})
+                rows = table.find('tbody').find_all('tr')
+                # 返回的lamdba表达式的详细内容
+                # for row in rows:
+                #     uncle_td = row.find_all('td')[1]
+                #     href = uncle_td.find('a')['href']
+                return [row.find_all('td')[1].find('a')['href'] for row in rows]
+            except Exception as e:
+                print(f"{e}\nreloading {url}")
 
     @staticmethod
     def load_detail_page(url):
         print(f"loading {url}")
-        content = requests.get(url).content
-        soup = BeautifulSoup(content, 'html.parser')
-        table = soup.find('div', {'class': 'card'}).find('div', {'class': 'card-body'})
-        info = {}
-        divs = table.find_all('div', {'class': 'col-md-9'})
-        info['uncleHeight'] = divs[0].find('strong').string
-        info['unclePosition'] = divs[1].string.strip('\n')
-        info['blockHeight'] = divs[2].find('a').string
-        info['hash'] = divs[3].string.strip('\n')
-        info['parentHash'] = divs[4].string.strip('\n')
-        info['sha3Uncles'] = divs[5].string.strip('\n')
-        info['minerHash'] = divs[6].find('a').string.split(' ')[0]
-        info['miner'] = divs[6].find('a').string.split(' ')[1] if len(divs[6].find('a').string.split(' ')) > 1 else ''
-        info['mineTime'] = divs[6].text.split('in ')[1].split(' secs')[0]
-        info['difficulty'] = divs[7].string.strip('\n')
-        info['gasLimit'] = divs[8].string.strip('\n')
-        info['gasUsed'] = divs[9].string.strip('\n')
-        info['timeStampUnformated'] = str(divs[10]).split('\n')[-2].split('(')[1].strip(')')
-        info['uncleReward'] = str(divs[11]).split('\n')[-2].replace('<b>', '').replace('</b>', '')
-        return info
+        while True:
+            try:
+                content = requests.get(url).content
+                soup = BeautifulSoup(content, 'html.parser')
+                table = soup.find('div', {'class': 'card'}).find('div', {'class': 'card-body'})
+                return extract_detail_page(table)
+            except Exception as e:
+                print(f"{e}\nreloading {url}")
 
     @staticmethod
     def save(info):
@@ -77,9 +87,11 @@ class UncleCrawler:
             f.write(s[:-1] + '\n')
 
     def test(self):
-        test_info = self.load_detail_page('https://etherscan.io/uncle/0x997b66eb0a1ac77433b07745a0e353708407c7fdc77e93b4cafc0d64f108c040')
+        test_info = self.load_detail_page(
+            'https://etherscan.io/uncle/0x997b66eb0a1ac77433b07745a0e353708407c7fdc77e93b4cafc0d64f108c040')
         print(test_info['timeStampUnformated'].split(' '))
 
+
 if __name__ == '__main__':
-    # UncleCrawler().start()
-    UncleCrawler().test()
+    UncleCrawler().start()
+    # UncleCrawler().test()
