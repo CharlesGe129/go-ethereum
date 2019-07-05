245f2800ece7cb09aa378fa44990733d743b6885
John1001Song <jinyuesong88@gmail.com>
2019-07-05 13:18:15 -0700

    update tokens; checkpoint download manually

--- analysis_tool_python/crawler/canonical_api.py
+++ analysis_tool_python/crawler/canonical_api.py
@@ -7,16 +7,19 @@ import math
 class CanonicalCrawler:
     def __init__(self):
         self.base_url = 'https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag={}&boolean=false&apikey={}'
-        self.api_keys = ['MRIQUZZTIFUAT725VCH6G369Z4MB2GU2P', 'YAZYM9PGKFUAY3XHYM162JB2BV9SB1F7IE', 'IX56CFH7GUYEWS1CMK9HBFDEMHJIXKG5S']
+        self.api_keys = ['MRIQUZZTIFUAT725VCH6G369Z4MB2GU2P', 'YAZYM9PGKFUAY3XHYM162JB2BV9SB1F7IE',
+                         'IX56CFH7GUYEWS1CMK9HBFDEMHJIXKG5S', 'XKKHZVZHRXWKVG6R1SR5Q46T5J2UKFCPAN',
+                         'R7WTQ3ZGZ6313ZG5KFZJRCRFYXW7MG37SR', 'VENFADVU89YCV8DYYBT2PRT7WF2WQQVJU4']
 
     def start(self):
         started_at = datetime.now()
-        for i in range(8000001, 1, -1):
+        # overall from 8000001
+        for i in range(7992878, 1, -1):
             # api_key = self.api_keys[random.randint(0, len(self.api_keys)-1)]
-            api_key = self.api_keys[i%3] # take reminder of 3
+            api_key = self.api_keys[i%6] # take reminder of 6
             self.load_page(self.base_url.format(hex(i), api_key))
             # return
-            print(f"Average time for {i} blocks: {((datetime.now() - started_at).seconds) / (8000002-i)} seconds")
+            print(f"Average time for {i} blocks: {((datetime.now() - started_at).seconds) / (7992879-i)} seconds")
 
     def load_page(self, url):
         print(url)
