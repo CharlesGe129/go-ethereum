aef8a4ba65ef13c0fb94fe0f163243c8673467c2
CharlesGe129 <char1209ge@gmail.com>
2019-07-05 21:20:40 +0800

    load canonical_api, save json into files

---
+++ analysis_tool_python/crawler/canonical_api.py
@@ -0,0 +1,34 @@
+import json
+import random
+import requests
+
+
+class CanonicalCrawler:
+    def __init__(self):
+        self.base_url = 'https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag={}&boolean=true&apikey={}'
+        self.api_keys = ['MRIQUZZTIFUAT725VCH6G369Z4MB2GU2P', 'YAZYM9PGKFUAY3XHYM162JB2BV9SB1F7IE', 'IX56CFH7GUYEWS1CMK9HBFDEMHJIXKG5S']
+
+    def start(self):
+        for i in range(8000000, 1, -1):
+            api_key = self.api_keys[random.randint(0, len(self.api_keys)-1)]
+            self.load_page(self.base_url.format(hex(i), api_key))
+            api_key = self.api_keys[random.randint(0, len(self.api_keys)-1)]
+            self.load_page(self.base_url.format(hex(i), api_key))
+            return
+
+    def load_page(self, url):
+        print(url)
+        content = requests.get(url).content
+        data = json.loads(content)
+        self.save(data)
+
+    def save(self, data):
+        height = int(data['result']['number'], 16)
+        filename = f'./canonical/{int(height/10000)*10000}.txt'
+        with open(filename, 'a') as f:
+            f.write(json.dumps(data['result']))
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    CanonicalCrawler().start()
