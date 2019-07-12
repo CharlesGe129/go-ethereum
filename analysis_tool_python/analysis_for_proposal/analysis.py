86294f59205d848799a42bbebdabea9c2470f2d2
John1001Song <jinyuesong88@gmail.com>
2019-07-12 08:42:14 -0700

    merge Charles code

--- analysis_tool_python/analysis_for_proposal/analysis.py
+++ analysis_tool_python/analysis_for_proposal/analysis.py
@@ -1,5 +1,6 @@
 import json
 import os
+from multiprocessing import Pool
 from _datetime import datetime, timezone
 
 '''
@@ -14,6 +15,26 @@ EJ Jung [12:13 PM]
 '''
 UNCLE_PATH = '../crawler/uncle_sample/'
 CANONICAL_PATH = '../crawler/canonical_sample/'
+
+examlpe_one_uncle = {"difficulty": "0x757be559a915a",
+                     "extraData": "0x65746865726d696e652d7061726974792d6e6f64652d6575",
+                     "gasLimit": "0x7a121d",
+                     "gasUsed": "0x79fae8",
+                     "hash": "0x5624997a439f66fe9548c283ffc312bbbeb0237f9fda22c1f4ab62395e695aea",
+                     "logsBloom": "0xa3cd9866b1569852c07e84024083dd8438d029c143810610c03d0c08648930782034262171c4a94de1809028c869dc5710204c1216102762ea99a48cfde1f00d0d70b8c083144432c979b288d20e1167280d64cd24542110c506bf79bb01680e433409ca7b7937431a386900c2c4de14364411a19900a04f9515169ba4bb2475a4b821ec4099208aaf1749df7a34399639ad9050820d6e22fddf297195f8959a3fc0e02181ea9b81960c03a0c15d16cbc1b686408f07a44203a88b0acdbd6cb25418291b2c4e0282c81864148c4464fb53456f9d282584422258634b096aa69dcd14f2c34528122ccda1b309c84af20865c2f07245d02a2bc9667a6d578e061c",
+                     "miner": "0xea674fdde714fd979de3edf0f56aa9716b898ec8",
+                     "mixHash": "0xbcc8b22765ff402b620d6fc78e42e7f754bddaaaf86ad222492fd01c418bc50b",
+                     "nonce": "0x4cbfa56022370c59",
+                     "number": "0x772bcd",
+                     "parentHash": "0x44dd910602befcb6d6e1bd4c7d1dc727e9fb77ebd750134b1c8b4692d18f636c",
+                     "receiptsRoot": "0x4eefb82608c9130cb3bc9f1077f293c425f5ead6cecc191968cd53dc402ffc39",
+                     "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+                     "size": "0x221",
+                     "stateRoot": "0x300ceb70dd73fa66ebc978295c7b7f096ac3cd2a4e3aaef0a3d0775285427ce9",
+                     "timestamp": "0x5ce55405",
+                     "totalDifficulty": "0x22e6573d84f936e19e9",
+                     "transactionsRoot": "0x486cf741ca62002401469dcab1789516b4f04b43aab2b65378940caaaecc9c5e",
+                     "uncles": []}
 
 
 def reverse_dict(data):
@@ -39,27 +60,18 @@ class Analysis:
         self.load_uncles()
         self.count_uncles()
 
+    def load_uncles(self):
+        for filename in os.listdir(UNCLE_PATH):
+            if filename.endswith(".txt"):
+                print(filename)
+                with open(UNCLE_PATH + filename) as f:
+                    while True:
+                        line = f.readline()
+                        if not line:
+                            break
+                        self.load_one_uncle(line)
+
     def load_one_uncle(self, content):
-        # one example
-        # {"difficulty": "0x757be559a915a",
-        # "extraData": "0x65746865726d696e652d7061726974792d6e6f64652d6575",
-        #  "gasLimit": "0x7a121d",
-        #  "gasUsed": "0x79fae8",
-        #  "hash": "0x5624997a439f66fe9548c283ffc312bbbeb0237f9fda22c1f4ab62395e695aea",
-        #  "logsBloom": "0xa3cd9866b1569852c07e84024083dd8438d029c143810610c03d0c08648930782034262171c4a94de1809028c869dc5710204c1216102762ea99a48cfde1f00d0d70b8c083144432c979b288d20e1167280d64cd24542110c506bf79bb01680e433409ca7b7937431a386900c2c4de14364411a19900a04f9515169ba4bb2475a4b821ec4099208aaf1749df7a34399639ad9050820d6e22fddf297195f8959a3fc0e02181ea9b81960c03a0c15d16cbc1b686408f07a44203a88b0acdbd6cb25418291b2c4e0282c81864148c4464fb53456f9d282584422258634b096aa69dcd14f2c34528122ccda1b309c84af20865c2f07245d02a2bc9667a6d578e061c",
-        #  "miner": "0xea674fdde714fd979de3edf0f56aa9716b898ec8",
-        #  "mixHash": "0xbcc8b22765ff402b620d6fc78e42e7f754bddaaaf86ad222492fd01c418bc50b",
-        #  "nonce": "0x4cbfa56022370c59",
-        #  "number": "0x772bcd",
-        #  "parentHash": "0x44dd910602befcb6d6e1bd4c7d1dc727e9fb77ebd750134b1c8b4692d18f636c",
-        #  "receiptsRoot": "0x4eefb82608c9130cb3bc9f1077f293c425f5ead6cecc191968cd53dc402ffc39",
-        #  "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-        #  "size": "0x221",
-        #  "stateRoot": "0x300ceb70dd73fa66ebc978295c7b7f096ac3cd2a4e3aaef0a3d0775285427ce9",
-        #  "timestamp": "0x5ce55405",
-        #  "totalDifficulty": "0x22e6573d84f936e19e9",
-        #  "transactionsRoot": "0x486cf741ca62002401469dcab1789516b4f04b43aab2b65378940caaaecc9c5e",
-        #  "uncles": []}
         data = json.loads(content)
         self.uncles[data['hash']] = data
         t = datetime.fromtimestamp(int(data['timestamp'], 16), timezone.utc)
@@ -71,24 +83,13 @@ class Analysis:
         self.append_freq(self.minute_freq, minute)
         self.append_freq(self.overall_hour_freq, t.hour)
         self.append_freq(self.overall_minute_freq, t.minute)
-        self.append_freq(self.overall_weekday_freq, t.weekday()+1)
+        self.append_freq(self.overall_weekday_freq, t.weekday() + 1)
 
     @staticmethod
     def append_freq(data, k):
         if k not in data:
             data[k] = 0
         data[k] += 1
-
-    def load_uncles(self):
-        for filename in os.listdir(UNCLE_PATH):
-            if filename.endswith(".txt"):
-                print(filename)
-                with open(UNCLE_PATH + filename) as f:
-                    while True:
-                        line = f.readline()
-                        if not line:
-                            break
-                        self.load_one_uncle(line)
 
     def count_uncles(self):
         print('In sample dataset: \n'
@@ -138,5 +139,5 @@ class Analysis:
 if __name__ == '__main__':
     a = Analysis()
     a.start()
-    a.test()
+    # a.test()
     # a.get_uncles_frequence()
--- analysis_tool_python/crawler/uncle_api.py
+++ analysis_tool_python/crawler/uncle_api.py
@@ -1,7 +1,10 @@
 import json
 import random
 import requests
+from functools import partial
+from multiprocessing import Pool
 from datetime import datetime
+
 
 class UncleCrawler:
     def __init__(self):
@@ -12,38 +15,49 @@ class UncleCrawler:
 
     def start(self):
         started_at = datetime.now()
-        i = 7788749
+        i = 7800000
+        pool = Pool()
+        # pool.map(partial(self.loop_10000_pages, thread_num=j), [i])
         while True:
+            pool.map(self.loop_10000_pages, [i, i + 10000, i + 20000, i + 30000, i + 40000])
+            i -= 50000
+
+    def loop_10000_pages(self, start_num):
+        i = 0
+        started_at = datetime.now()
+        while i < 10000:
             try:
-                api_key = self.api_keys[random.randint(0, len(self.api_keys)-1)]
-                # data = self.load_page(self.base_url.format(hex(i), api_key))
-                self.load_uncles(api_key, self.base_url, i)
-                print(f"Average time for {i} blocks: {((datetime.now() - started_at).seconds) / (7788750-i)} seconds\n")
-                i -= 1
+                page_num = start_num + i
+                api_key = self.api_keys[random.randint(0, len(self.api_keys) - 1)]
+                self.load_uncles(api_key, self.base_url, page_num)
+                print(
+                    f"Started_num={start_num}: Avg time for {i+1} blocks: {((datetime.now() - started_at).seconds) / (i+1)} seconds\n")
+                i += 1
             except Exception as e:
                 print(f'Exception {e}. \nRetrying...')
 
     def load_uncles(self, api_key, base_url, uncle_number):
         uncle_index = 0
         while True:
-            data = self.load_page(base_url.format(hex(uncle_number), hex(uncle_index), api_key))
-            if data['result'] == None:
+            url = base_url.format(hex(uncle_number), hex(uncle_index), api_key)
+            data = self.load_page(url)
+            if not data['result']:
                 return
             else:
                 self.save(data)
                 uncle_index += 1
 
-
-    def load_page(self, url):
+    @staticmethod
+    def load_page(url):
         print(url)
         content = requests.get(url).content
         data = json.loads(content)
         return data
-        # self.save(data)
 
-    def save(self, data):
+    @staticmethod
+    def save(data):
         height = int(data['result']['number'], 16)
-        filename = f'./uncle/{int(height/10000)*10000}.txt'
+        filename = f'./uncle/{int(height / 10000) * 10000}.txt'
         with open(filename, 'a') as f:
             f.write(json.dumps(data['result']))
             f.write('\n')
