41d72e259979d3830f08396d8ceff744a6c91744
CharlesGe129 <char1209ge@gmail.com>
2019-08-04 12:00:13 +0800

    Add utils. Add bc_to_json(broadcast).

---
+++ analysis_tool_python/util/load_file.py
@@ -0,0 +1,38 @@
+import os
+
+
+def get_file_lines(path, filename, mode='r'):
+    print('loading ' + path + filename)
+    with open(path + filename, mode) as f:
+        return f.readlines()
+
+
+def load_file_yield_lines(path, filename, mode='r'):
+    print('loading ' + path + filename)
+    with open(path + filename, mode) as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            elif line == '\n':
+                continue
+            yield line.strip("\n")
+
+
+def load_path(path):
+    for filename in sorted(os.listdir(path)):
+        if "ds_store" in filename.lower():
+            continue
+        yield filename
+
+
+def load_field(line, field):
+    if field in line:
+        return line.split(f'{field}=')[1].split(',')[0]
+    else:
+        return ''
+
+
+def check_dir_exist(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
---
+++ analysis_tool_python/util/time_format.py
@@ -0,0 +1,9 @@
+import time
+from datetime import datetime, timedelta
+
+
+def load_time_to_utc_unix(time_str, format):
+    t = datetime.strptime(time_str.split(' +')[0], format)
+    if "+0800" in time_str or "CST" in time_str:
+        t = t + timedelta(hours=-8)
+    return time.mktime(t.timetuple())
