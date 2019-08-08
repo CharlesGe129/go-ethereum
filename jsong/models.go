687623a04d8f7960640e13a0b5cc3253dd1d47ce
yj.ge <yj.ge@tianrang-inc.com>
2019-08-08 13:24:56 +0800

    record uncles' hash

--- jsong/models.go
+++ jsong/models.go
@@ -3,6 +3,7 @@ package jsong
 import (
 	"encoding/hex"
 	"encoding/json"
+
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
@@ -11,28 +12,32 @@ type Transaction struct {
 }
 
 type Block struct {
-	Hash            string `json:"hash"`
-	ParentHash      string `json:"parentHash"`
-	UncleHash       string `json:"uncleHash"`
-	ReceiptHash     string `json:"receiptHash"`
-	Nonce           uint64 `json:"nonce"`
-	LogsBloom       string `json:"logsBloom"`
-	Number          string `json:"number"`
-	Miner           string `json:"miner"`
-	UncleNum        int    `json:"uncleNum"`
-	TxNum           int    `json:"txNum"`
-	GasUsed         uint64 `json:"gasUsed"`
-	GasLimit        uint64 `json:"gasLimit"`
-	Difficulty      string `json:"difficulty"`
-	Root            string `json:"root"`
-	MixDigest       string `json:"mixDigest"`
-	Size            string `json:"size"`
-	TotalDifficulty string `json:"totalDifficulty"`
-	Extra           string `json:"extra"`
-	Timestamp       uint64 `json:"timestamp"`
+	Hash            string   `json:"hash"`
+	ParentHash      string   `json:"parentHash"`
+	UncleHash       string   `json:"uncleHash"`
+	ReceiptHash     string   `json:"receiptHash"`
+	Nonce           uint64   `json:"nonce"`
+	LogsBloom       string   `json:"logsBloom"`
+	Number          string   `json:"number"`
+	Miner           string   `json:"miner"`
+	Uncles          []string `json:"uncles"`
+	TxNum           int      `json:"txNum"`
+	GasUsed         uint64   `json:"gasUsed"`
+	GasLimit        uint64   `json:"gasLimit"`
+	Difficulty      string   `json:"difficulty"`
+	Root            string   `json:"root"`
+	MixDigest       string   `json:"mixDigest"`
+	Size            string   `json:"size"`
+	TotalDifficulty string   `json:"totalDifficulty"`
+	Extra           string   `json:"extra"`
+	Timestamp       uint64   `json:"timestamp"`
 }
 
 func BlockToJson(block *types.Block) string {
+	var uncles []string
+	for _, uncle := range block.Uncles() {
+		uncles = append(uncles, uncle.Hash().String())
+	}
 	b := Block{
 		Hash:            block.Hash().String(),
 		ParentHash:      block.ParentHash().String(),
@@ -42,7 +47,7 @@ func BlockToJson(block *types.Block) string {
 		LogsBloom:       hex.EncodeToString(block.Bloom().Bytes()),
 		Number:          block.Number().String(),
 		Miner:           block.Header().Coinbase.String(),
-		UncleNum:        len(block.Uncles()),
+		Uncles:          uncles,
 		TxNum:           len(block.Transactions()),
 		GasUsed:         block.GasUsed(),
 		GasLimit:        block.GasLimit(),
