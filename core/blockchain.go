e312c7c8de2aaef8ee379c2d00ebb8f5eb251b40
CharlesGe129 <char1209ge@gmail.com>
2019-08-26 21:36:54 +0800

    Remove unused code

--- core/blockchain.go
+++ core/blockchain.go
@@ -1685,23 +1685,6 @@ func (bc *BlockChain) insertChain(chain types.Blocks, verifySeals bool) (int, []
 	return it.index, events, coalescedLogs, err
 }
 
-func appendToFile(fileName string, content string) error {
-	// 以只写的模式，打开文件
-	f, err := os.OpenFile(fileName, os.O_WRONLY, 0644)
-	if err != nil {
-		os.Create(fileName)
-		f, err = os.OpenFile(fileName, os.O_WRONLY, 0644)
-	}
-
-	// 查找文件末尾的偏移量
-	n, _ := f.Seek(0, os.SEEK_END)
-	// 从末尾的偏移量开始写入内容
-	_, err = f.WriteAt([]byte(content), n)
-
-	defer f.Close()
-	return err
-}
-
 // insertSideChain is called when an import batch hits upon a pruned ancestor
 // error, which happens when a sidechain with a sufficiently old fork-block is
 // found.
